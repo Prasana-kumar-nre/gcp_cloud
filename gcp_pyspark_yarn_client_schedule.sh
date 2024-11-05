24ac29335e57035c3d0f1f1aeca88ae77818a821
Prasana Kumar N R E <prasanakumarnre@gmail.com>
2024-11-05 16:10:53 +0530

    Update gcp_pyspark_yarn_client_schedule.sh

--- gcp_pyspark_yarn_client_schedule.sh
+++ gcp_pyspark_yarn_client_schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gcloud dataproc jobs submit pyspark --cluster=prasana-c02 --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g",
 "spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client",
-"spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/
+"spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/prasana08042000/.git/gcp_cloud/usecase_4.py
 
 if [$? -ne 0]
 then 
